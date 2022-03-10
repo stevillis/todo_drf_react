8c76fb40378d55777700d2e7ef4af7aa7cf44737
Stévillis Sousa <stevillis@hotmail.com>
2022-03-10 15:05:41 -0400

    Create HTML structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 class="text-center bg-info text-white rounded" id="pageTitle">To Do</h1>
+        <div id="task-container" className="border-rounded">
+          <div id="form-wrapper" className="border-rounded-top">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add task" />
+                </div>
+                <div style={{ flex: 1 }}>
+                  <button id="submit" className="btn btn-outline-success" type="submit">
+                    <i class="fa-solid fa-plus"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper" className='border-rounded-bottom'></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
