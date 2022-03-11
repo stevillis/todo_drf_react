a995371a9759aa23de203728dfd1cb81ededac6b
Stévillis Sousa <stevillis@hotmail.com>
2022-03-11 19:00:18 -0400

    Add Task

--- src/App.js
+++ src/App.js
@@ -24,6 +24,8 @@ class App extends Component {
     this.getCookie = this.getCookie.bind(this);
     this.formatDate = this.formatDate.bind(this);
     this.fetchTasks = this.fetchTasks.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -46,6 +48,35 @@ class App extends Component {
     return cookieValue;
   }
 
+  fetchTasks(requestURL, method, body) {
+    const csrftoken = this.getCookie('csrftoken');
+
+    console.log(body);
+    fetch(requestURL, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken
+      },
+      body: body
+    })
+      .then(response => response.json())
+      .then(tasks => {
+        if (method === 'GET') this.setState({ taskList: tasks });
+        else {
+          this.fetchTasks(`${API_URL}task-list/`, 'GET', null);
+          this.setState({
+            activeItem: {
+              id: null,
+              title: '',
+              completed: false
+            }
+          });
+        }
+      })
+      .catch(error => console.log('Error:', error));
+  }
+
   formatDate(date) {
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
@@ -57,23 +88,24 @@ class App extends Component {
     return `${day}/${month}/${year} ${hour}:${minute}:${second}`
   }
 
-  fetchTasks(requestURL, method, body) {
-    const csrftoken = this.getCookie('csrftoken');
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value)
 
-    fetch(requestURL, {
-      method: method,
-      headers: {
-        'Content-Type': 'application/json',
-        'X-CSRFToken': csrftoken
-      },
-      body: body
-    })
-      .then(response => response.json())
-      .then(tasks => {
-        this.setState({
-          taskList: tasks
-        });
-      });
+    this.setState({
+      activeItem: {
+        ...this.state.activeItem,
+        title: value
+      }
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(`${API_URL}task-create/`);
+    this.fetchTasks(`${API_URL}task-create/`, 'POST', JSON.stringify(this.state.activeItem));
   }
 
   render() {
@@ -83,10 +115,10 @@ class App extends Component {
         <h1 className="text-center bg-info text-white rounded" id="pageTitle">To Do</h1>
         <div id="task-container" className="border-rounded">
           <div id="form-wrapper" className="border-rounded-top">
-            <form id="form">
+            <form id="form" onSubmit={this.handleSubmit}>
               <div className="flex-wrapper">
                 <div style={{ flex: 6 }}>
-                  <input className="form-control" id="title" type="text" name="title" placeholder="Add task" />
+                  <input type="text" id="title" name="title" value={this.state.activeItem.title} className="form-control" placeholder="Add task" maxlength="60" onChange={this.handleChange} />
                 </div>
                 <div style={{ flex: 1 }}>
                   <button id="submit" className="btn btn-outline-success" type="submit">
@@ -100,41 +132,39 @@ class App extends Component {
           <div id="list-wrapper" className='border-rounded-bottom'>
             {
               tasks.map((task, index) => {
-                {
-                  const created = new Date(task.created);
-                  const updated = new Date(task.updated);
-                  const createdFormated = this.formatDate(created);
-                  const updatedFormated = this.formatDate(updated);
-
-                  return (
-                    <div key={index} id="data-row-${i}" className="task-wrapper flex-wrapper">
-                      <div style={{ flex: 7 }} className="data">
-                        {
-                          task.completed ?
-                            <span id="task-title" className="title line-through">{task.title}</span> :
-                            <span id="task-title" className="title">{task.title}</span>
-                        }
-                        <br />
-                        <small>
-                          Criada em: ${createdFormated}
-                        </small>
-                        {
-                          task.completed ? <span><br /><small>Concluída em: {updatedFormated}</small></span> : ''
-                        }
-                      </div>
-                      <div style={{ flex: 1 }} className="text-center">
-                        <button className="btn btn-sm btn-outline-info edit">
-                          <i className="fa-solid fa-pencil"></i>
-                        </button>
-                      </div>
-                      <div style={{ flex: 1 }} className="text-center">
-                        <button className="btn btn-sm btn-outline-danger delete">
-                          <i className="fa-solid fa-trash"></i>
-                        </button>
-                      </div>
+                const created = new Date(task.created);
+                const updated = new Date(task.updated);
+                const createdFormated = this.formatDate(created);
+                const updatedFormated = this.formatDate(updated);
+                const dataRowId = `data-row-${index}`;
+                return (
+                  <div key={index} id={dataRowId} className="task-wrapper flex-wrapper">
+                    <div style={{ flex: 7 }} className="data">
+                      {
+                        task.completed ?
+                          <span id="task-title" className="title line-through">{task.title}</span> :
+                          <span id="task-title" className="title">{task.title}</span>
+                      }
+                      <br />
+                      <small>
+                        Criada em: {createdFormated}
+                      </small>
+                      {
+                        task.completed ? <span><br /><small>Concluída em: {updatedFormated}</small></span> : ''
+                      }
                     </div>
-                  );
-                }
+                    <div style={{ flex: 1 }} className="text-center">
+                      <button className="btn btn-sm btn-outline-info edit">
+                        <i className="fa-solid fa-pencil"></i>
+                      </button>
+                    </div>
+                    <div style={{ flex: 1 }} className="text-center">
+                      <button className="btn btn-sm btn-outline-danger delete">
+                        <i className="fa-solid fa-trash"></i>
+                      </button>
+                    </div>
+                  </div>
+                );
               })
             }
           </div>
